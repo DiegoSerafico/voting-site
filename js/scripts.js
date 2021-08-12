ce33ff098e241506568e581bedeb17863a86b903
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-12 09:45:34 -0700

    add form and messages including functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   $("#age").submit(function(event) {
     event.preventDefault();
+    $("#ofAge").hide();
+    $("#minor").hide();
+    $("#warning").hide();
+    const age = parseInt($("#ageInput").val());
+    if (isNaN(age)) {
+      $("#warning").show();
+    } else if (age < 18) {
+      $("#minor").show();
+    } else {
+      $("#ofAge").show();
+    }
   });
 });
